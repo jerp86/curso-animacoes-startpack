06d8954d1f769f8266833ec921d02a9c9fcf4526
jerp86 <jerp4@hotmail.com>
2019-12-03 07:21:51 -0300

    Arrastando usuário

--- components/User.js
+++ components/User.js
@@ -7,18 +7,55 @@ import {
   Text,
   Image,
   Animated,
+  Alert,
   StyleSheet,
   Dimensions,
+  PanResponder,
   TouchableWithoutFeedback,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+const { width } = Dimensions.get('window');
 
 export default class User extends Component {
   state = {
     opacity: new Animated.Value(0),
     offset: new Animated.ValueXY({ x: 0, y: 50 }),
   };
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onPanResponderTerminationRequest: () => false,
+      onMoveShouldSetPanResponder: () => true,
+
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.offset.x,
+          dy: this.state.offset.y,
+        },
+      ]),
+
+      onPanResponderRelease: () => {
+        if (this.state.offset.x._value < -200) {
+          Alert.alert('Deleted!', 'Your post will be deleted');
+        }
+
+        Animated.spring(this.state.offset, {
+          toValue: 0,
+          bounciness: 30,
+        }).start();
+      },
+
+      onPanResponderTerminate: () => {
+        Animated.spring(this.state.offset, {
+          toValue: 0,
+          bounciness: 20,
+        }).start();
+      },
+    });
+  }
 
   componentDidMount() {
     Animated.parallel([
@@ -40,8 +77,19 @@ export default class User extends Component {
 
     return (
       <Animated.View
+        {...this._panResponder.panHandlers}
         style={[
-          { transform: [...this.state.offset.getTranslateTransform()] },
+          {
+            transform: [
+              ...this.state.offset.getTranslateTransform(),
+              {
+                rotateZ: this.state.offset.x.interpolate({
+                  inputRange: [width * -1, width],
+                  outputRange: ['-100deg', '100deg'],
+                }),
+              },
+            ],
+          },
           { opacity: this.state.opacity },
         ]}
       >
